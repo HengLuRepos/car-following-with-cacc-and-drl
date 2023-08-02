17700bcb0108f709f0666b0f1ea7ae488a217fba
henglu <1505613731@qq.com>
2023-08-02 19:01:36 -0400

    update training script

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 class Config:
     def __init__(self, max_acc = 3, tau=0.8, max_v=30):
-        self.buffer_size=100000
+        self.buffer_size=None
         self.batch_size=256
         self.gamma = 0.9
-        self.rho = 0.995
-        self.pi_lr=5e-4
-        self.v_lr = 5e-4
+        self.tau = 0.99
+        self.pi_lr=3e-4
+        self.v_lr = 3e-4
         self.explore_noise=0.1
         self.target_noise=0.2
         self.noise_clip=0.5
@@ -14,7 +14,7 @@ class Config:
 
         self.start_steps = 5000
         self.max_acc = max_acc
-        self.tau = tau
+        self.tau_t = tau #time
         self.max_v = max_v
 
         self.max_timestamp = 1000000
@@ -22,4 +22,5 @@ class Config:
         self.a_high = self.max_acc
         self.eval_freq = 5000
         self.eval_epoch = 1
+        self.update_freq = 1
         
--- test.py
+++ test.py
@@ -22,6 +22,8 @@ def eval():
         action = ddpg.actor(np2torch(following_temp.get_state())).detach().cpu().numpy()
         next_state, reward, terminated, truncated, _ = following_temp.step(action[0])
         done = terminated or truncated or ld_done
+        ld_acc = ld.get_acc()
+        following_temp.update(ld_acc)
         ep_reward += reward
         steps += 1
         rel_v.append(following_temp.get_state()[1] - following_temp.get_state()[0])
@@ -31,6 +33,7 @@ def eval():
     print(f"max rel_dist:{max(rel_d):.3f};  min rel_dist:{min(rel_d):.3f} mean rel_dist:{np.mean(rel_d):.3f}")
     print(f"max rel_v:{max(rel_v):.3f};  min rel_v:{min(rel_v):.3f} mean rel_v:{np.mean(rel_v):.3f}")
     print("---------------------------------------")
+    return ep_reward
     
 
 config = Config(max_acc=3)
@@ -43,27 +46,34 @@ episode_num = 0
 ld_state, _ = lead.reset()
 following_state, _ = following.reset()
 BETA = 0.5
+cacc = False
+episodic_reward_eval = None
 for t in range(config.max_timestamp):
     episode_timesteps += 1
     ld_action = lead.get_acc()
     following.update(ld_action)
+    state = following.get_state()
     if t < config.start_steps:
         following_action = np.abs(following.action_space.sample())
     else:
-        following_action = ddpg.actor.explore(np2torch(following.get_state())).detach().cpu().numpy()
+        following_action = ddpg.actor.explore(np2torch(state)).detach().cpu().numpy()
         current = following.get_state()
         cacc_action = np.array([current[0] + 0.2 * current[4] + 0.9*current[1]])
         if following.try_step(cacc_action[0]) > following.try_step(following_action[0]):
             #following_action = (1.0 - BETA) * cacc_action + BETA * following_action
             following_action = cacc_action
+        
     _, ld_done = lead.step()
     next_state, reward, terminated, truncated, _ = following.step(following_action[0])
+    next_acc = lead.get_acc()
+    next_state[3] = next_acc
     done = terminated or truncated or ld_done
-    ddpg.buffer.remember(following.get_state(), following_action, reward, next_state, done)
+    ddpg.buffer.remember(state, following_action, reward, next_state, done)
     episode_reward += reward
 
     if t >= config.start_steps:
-        ddpg.train_iter()
+        for i in range(config.update_freq):
+            ddpg.train_iter()
     if done:
         print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
         lead.reset()
@@ -72,6 +82,10 @@ for t in range(config.max_timestamp):
         episode_reward = 0
         episode_timesteps = 0
         episode_num += 1
+        ddpg.save_model("models/td3-cacc.pt")
     if (t + 1) % config.eval_freq == 0:
-        eval()
+        ep_reward = eval()
+        if episodic_reward_eval is None or ep_reward >= episodic_reward_eval:
+            episodic_reward_eval = ep_reward
+            ddpg.save_model("models/td3-cacc-best.pt")
         
